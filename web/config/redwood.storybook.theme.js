3c3929f20c95fe6e4b9e69c54f3083a3d52de9f8
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2022-03-29 23:38:02 -0400

    some clean up (#16)
    * doc: add netlify badge
    
    * feat(storybook): add Redwood theme
    
    * chore(github): ignore building for some paths

---
+++ web/config/redwood.storybook.theme.js
@@ -0,0 +1,22 @@
+import { create } from '@storybook/theming'
+
+export default create({
+  base: 'light',
+  colorPrimary: '#bf4722',
+  colorSecondary: '#341309',
+  barSelectedColor: '#faeae5',
+  textColor: '#682712',
+  appBg: '#faeae5',
+  barBg: '#bf4722',
+  barTextColor: '#f3c7ba',
+  appContentBg: '#faeae5',
+  textMutedColor: '#341309',
+  appBorderColor: '#faeae5',
+  textInverseColor: '#faeae5',
+  fontBase:
+    'Open Sans,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,sans-serif',
+  brandTitle: 'RedwoodJS',
+  brandUrl: 'https://redwoodjs.com/',
+  appBorderRadius: 16,
+  brandImage: 'diecut.svg',
+})

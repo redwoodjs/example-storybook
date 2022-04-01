e20c845eda235f9fac17c227567ebc95e69b4c34
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2022-04-01 14:14:38 -0400

    chore: add styling

--- web/src/components/Comment/Comment.js
+++ web/src/components/Comment/Comment.js
@@ -1,9 +1,19 @@
+const formattedDate = (datetime) => {
+  const parsedDate = new Date(datetime)
+  const month = parsedDate.toLocaleString('default', { month: 'long' })
+  return `${parsedDate.getDate()} ${month} ${parsedDate.getFullYear()}`
+}
+
 const Comment = ({ comment }) => {
   return (
-    <div>
-      <h2>{comment.name}</h2>
-      <time dateTime={comment.createdAt}>{comment.createdAt}</time>
-      <p>{comment.body}</p>
+    <div className="bg-gray-200 p-8 rounded-lg">
+      <header className="flex justify-between">
+        <h2 className="font-semibold text-gray-700">{comment.name}</h2>
+        <time className="text-xs text-gray-500" dateTime={comment.createdAt}>
+          {formattedDate(comment.createdAt)}
+        </time>
+      </header>
+      <p className="text-sm mt-2">{comment.body}</p>
     </div>
   )
 }

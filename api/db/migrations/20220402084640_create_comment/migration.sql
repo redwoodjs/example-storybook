95d4435b169ab7252b6ab06b13dbc1bb066e6c48
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2022-04-02 04:57:45 -0400

    chore(prisma): generate Comment table migration

---
+++ api/db/migrations/20220402084640_create_comment/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "postId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

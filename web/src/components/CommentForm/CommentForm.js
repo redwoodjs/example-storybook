836aed08dd9e60a9eba8750df42f4bf8289d9e30
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2022-04-02 05:19:25 -0400

    chore(commentForm): add submit

--- web/src/components/CommentForm/CommentForm.js
+++ web/src/components/CommentForm/CommentForm.js
@@ -1,32 +1,68 @@
-import { Form, Label, TextField, TextAreaField, Submit } from '@redwoodjs/forms'
+import {
+  Form,
+  FormError,
+  Label,
+  TextField,
+  TextAreaField,
+  Submit,
+} from '@redwoodjs/forms'
+import { useMutation } from '@redwoodjs/web'
+
+const CREATE = gql`
+  mutation CreateCommentMutation($input: CreateCommentInput!) {
+    createComment(input: $input) {
+      id
+      name
+      body
+      createdAt
+    }
+  }
+`
 
 const CommentForm = () => {
+  const [createComment, { loading, error }] = useMutation(CREATE)
+
+  const onSubmit = (input) => {
+    createComment({ variables: { input } })
+  }
+
   return (
     <div>
       <h3 className="font-light text-lg text-gray-600">Leave a Comment</h3>
-      <Form className="mt-4 w-full">
-        <Label name="name" className="block text-sm text-gray-600 uppercase">
+      <Form className="mt-4 w-full" onSubmit={onSubmit}>
+        <FormError
+          error={error}
+          titleClassName="font-semibold"
+          wrapperClassName="bg-red-100 text-red-900 text-sm p-3 rounded"
+        />
+        <Label
+          name="name"
+          className="block text-xs font-semibold text-gray-500 uppercase"
+        >
           Name
         </Label>
         <TextField
           name="name"
-          className="block w-full p-1 border rounded text-xs "
+          className="block w-full p-1 border rounded text-sm "
           validation={{ required: true }}
         />
 
         <Label
           name="body"
-          className="block mt-4 text-sm text-gray-600 uppercase"
+          className="block mt-4 text-xs font-semibold text-gray-500 uppercase"
         >
           Comment
         </Label>
         <TextAreaField
           name="body"
-          className="block w-full p-1 border rounded h-24 text-xs"
+          className="block w-full p-1 border rounded h-24 text-sm"
           validation={{ required: true }}
         />
 
-        <Submit className="block mt-4 bg-blue-500 text-white text-xs font-semibold uppercase tracking-wide rounded px-3 py-2 disabled:opacity-50">
+        <Submit
+          disabled={loading}
+          className="block mt-4 bg-blue-500 text-white text-xs font-semibold uppercase tracking-wide rounded px-3 py-2 disabled:opacity-50"
+        >
           Submit
         </Submit>
       </Form>

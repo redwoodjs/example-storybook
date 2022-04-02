a66e22b3c2099b47c969725dd1e00fc9a16fa5c0
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2022-04-02 05:27:12 -0400

    chore(storybook): wire up mockGraphQLMutation

--- web/src/components/CommentForm/CommentForm.stories.js
+++ web/src/components/CommentForm/CommentForm.stories.js
@@ -1,6 +1,20 @@
 import CommentForm from './CommentForm'
 
 export const generated = (args) => {
+  mockGraphQLMutation('CreateCommentMutation', (variables, { ctx }) => {
+    const id = parseInt(Math.random() * 1000)
+    ctx.delay(1000)
+
+    return {
+      createComment: {
+        id,
+        name: variables.input.name,
+        body: variables.input.body,
+        createdAt: new Date().toISOString(),
+      },
+    }
+  })
+
   return <CommentForm {...args} />
 }
 

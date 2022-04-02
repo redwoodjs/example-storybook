6dbcd60c52b53863e4594d7504f8db9aaeacdd6e
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2022-04-02 05:17:46 -0400

    chore(commentForm): simple form

--- web/src/components/CommentForm/CommentForm.js
+++ web/src/components/CommentForm/CommentForm.js
@@ -1,8 +1,35 @@
+import { Form, Label, TextField, TextAreaField, Submit } from '@redwoodjs/forms'
+
 const CommentForm = () => {
   return (
     <div>
-      <h2>{'CommentForm'}</h2>
-      <p>{'Find me in ./web/src/components/CommentForm/CommentForm.js'}</p>
+      <h3 className="font-light text-lg text-gray-600">Leave a Comment</h3>
+      <Form className="mt-4 w-full">
+        <Label name="name" className="block text-sm text-gray-600 uppercase">
+          Name
+        </Label>
+        <TextField
+          name="name"
+          className="block w-full p-1 border rounded text-xs "
+          validation={{ required: true }}
+        />
+
+        <Label
+          name="body"
+          className="block mt-4 text-sm text-gray-600 uppercase"
+        >
+          Comment
+        </Label>
+        <TextAreaField
+          name="body"
+          className="block w-full p-1 border rounded h-24 text-xs"
+          validation={{ required: true }}
+        />
+
+        <Submit className="block mt-4 bg-blue-500 text-white text-xs font-semibold uppercase tracking-wide rounded px-3 py-2 disabled:opacity-50">
+          Submit
+        </Submit>
+      </Form>
     </div>
   )
 }

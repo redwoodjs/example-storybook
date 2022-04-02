266dbfb8cfed3f65cd9ff9dce5a38e1970b127b5
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2022-04-02 05:14:50 -0400

    chore(commentForm): run generator
    `yarn rw g component CommentForm`
    https://redwoodjs.com/docs/tutorial/chapter6/comment-form

---
+++ web/src/components/CommentForm/CommentForm.js
@@ -0,0 +1,10 @@
+const CommentForm = () => {
+  return (
+    <div>
+      <h2>{'CommentForm'}</h2>
+      <p>{'Find me in ./web/src/components/CommentForm/CommentForm.js'}</p>
+    </div>
+  )
+}
+
+export default CommentForm
---
+++ web/src/components/CommentForm/CommentForm.stories.js
@@ -0,0 +1,7 @@
+import CommentForm from './CommentForm'
+
+export const generated = () => {
+  return <CommentForm />
+}
+
+export default { title: 'Components/CommentForm' }

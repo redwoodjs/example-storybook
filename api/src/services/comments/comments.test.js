bb41f32fcd8338d5c16e160bb831cff9e201caca
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2022-04-02 07:17:33 -0400

    chore: get comment by post

--- api/src/services/comments/comments.test.js
+++ api/src/services/comments/comments.test.js
@@ -1,4 +1,5 @@
 import { comments, createComment } from './comments'
+import { db } from 'src/lib/db'
 
 // Generated boilerplate tests do not account for all circumstances
 // and can fail without adjustments, e.g. Float and DateTime types.
@@ -7,11 +8,17 @@ import { comments, createComment } from './comments'
 // https://redwoodjs.com/docs/testing#jest-expect-type-considerations
 
 describe('comments', () => {
-  scenario('returns all comments', async (scenario) => {
-    const result = await comments()
-
-    expect(result.length).toEqual(Object.keys(scenario.comment).length)
-  })
+  scenario(
+    'returns all comments for a single post from the database',
+    async (scenario) => {
+      const result = await comments({ postId: scenario.comment.jane.postId })
+      const post = await db.post.findUnique({
+        where: { id: scenario.comment.jane.postId },
+        include: { comments: true },
+      })
+      expect(result.length).toEqual(post.comments.length)
+    }
+  )
 
   scenario('postOnly', 'creates a new comment', async (scenario) => {
     const comment = await createComment({

411a76fea3fdf34ba3c7062f00fa9a738275449c
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2022-06-19 14:27:13 -0400

    feat: add MDX story

--- web/config/storybook.config.js
+++ web/config/storybook.config.js
@@ -1,6 +1,15 @@
+const { importStatementPath, getPaths } = require('@redwoodjs/internal')
+
+const rwjsPaths = getPaths()
+
 module.exports = {
   features: {
     interactionsDebugger: true,
   },
   addons: ['@storybook/addon-interactions'],
+  stories: [
+    `${importStatementPath(
+      rwjsPaths.web.src
+    )}/stories/Introduction.stories.mdx`,
+  ],
 }

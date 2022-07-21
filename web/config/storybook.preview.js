52145b15c3639d904451ea3d3036cea5172583bb
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2022-07-21 17:50:05 -0400

    feat(storybook): add some configuration
     incorporate part's of @realStandal's gist
    
    - https://github.com/redwoodjs/example-storybook/issues/117#issuecomment-1189636124
    - https://gist.github.com/realStandal/b93086e12d687932c13a39ca49e3c3f9
    
    - [ ] Toggling my application's MainLayout using a custom toolbar menu - this Layout gives my app its dashboard-like appearance.
    - [ ] Add my application's custom Toaster implementation, to sync my Storybook toast-notifications with my app's.
    - [X] Customizing the order stories are sorted in the sidebar.
    - [X] Bringing in my custom theme and applying it to Storybook's docs plugin.

--- web/config/storybook.preview.js
+++ web/config/storybook.preview.js
@@ -1,2 +1,34 @@
 import '../src/scaffold.css'
 import '../src/index.css'
+
+// https://github.com/redwoodjs/example-storybook/issues/117#issuecomment-1189636124
+// https://gist.github.com/realStandal/b93086e12d687932c13a39ca49e3c3f9
+import { theme } from './storybook.manager'
+
+export const globalTypes = {
+  layout: {
+    name: 'Layout',
+    description: 'Toggle the main layout wrapper.',
+    defaultValue: 'on',
+    toolbar: {
+      icon: 'component',
+      items: ['off', 'on'],
+    },
+  },
+}
+
+const order = [
+  'Pages',
+  ['Auth', 'Meta'],
+  'Layouts',
+  'Cells',
+  'Components',
+]
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  backgrounds: { disable: true },
+  layout: 'fullscreen',
+  docs: { theme },
+  options: { storySort: { order } },
+}

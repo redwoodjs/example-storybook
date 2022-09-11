ca7d79b47827283c179159f66df10f0f1a01e955
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2022-09-10 20:34:07 -0400

    feat(theme-ui): wire up
    https://theme-ui.com/getting-started

--- web/src/App.js
+++ web/src/App.js
@@ -1,3 +1,6 @@
+// /** @jsxImportSource theme-ui */
+import { ThemeProvider } from 'theme-ui'
+import { theme } from './theme'
 import { AuthProvider } from '@redwoodjs/auth'
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
@@ -8,15 +11,29 @@ import Routes from 'src/Routes'
 import './scaffold.css'
 import './index.css'
 
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <AuthProvider type="dbAuth">
-        <RedwoodApolloProvider>
-          <Routes />
-        </RedwoodApolloProvider>
-      </AuthProvider>
-    </RedwoodProvider>
+    <ThemeProvider theme={theme}>
+      <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
+        <AuthProvider type="dbAuth">
+          <RedwoodApolloProvider>
+            <div
+              sx={{
+                fontWeight: 'bold',
+                fontSize: 4, // picks up value from `theme.fontSizes[4]`
+                color: 'primary', // picks up value from `theme.colors.primary`
+              }}
+            >
+              Hello Theme UI
+            </div>
+            <Routes />
+          </RedwoodApolloProvider>
+        </AuthProvider>
+      </RedwoodProvider>
+    </ThemeProvider>
   </FatalErrorBoundary>
 )
 
---
+++ web/src/theme.js
@@ -0,0 +1,15 @@
+// import type { Theme } from 'theme-ui'
+
+// export const theme: Theme = {
+export const theme = {
+  fonts: {
+    body: 'system-ui, sans-serif',
+    heading: '"Avenir Next", sans-serif',
+    monospace: 'Menlo, monospace',
+  },
+  colors: {
+    text: '#000',
+    background: '#fff',
+    primary: '#33e',
+  },
+}

8f5b93f12d3692b06332ccacec42c46fe28e05c6
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2022-10-03 22:56:50 -0400

    chore(jest): config to help CI pass?
    see also:
    - https://github.com/facebook/jest/issues/2663#issuecomment-317109798
    - https://github.com/facebook/jest/issues/11739#issuecomment-896693041

--- web/jest.config.js
+++ web/jest.config.js
@@ -10,6 +10,9 @@ const config = {
     '~__REDWOOD__USER_WEB_DEFAULT_CSS': 'identity-obj-proxy',
     '~__REDWOOD__USER_STORYBOOK_PREVIEW_CONFIG':
       '<rootDir>/web/config/storybook.preview.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/assetsTransformer.js',
+    '\\.(css|less)$': '<rootDir>/assetsTransformer.js',
   },
   transform: {
     '^.+\\.mdx?$': '@storybook/addon-docs/jest-transform-mdx', // https://github.com/storybookjs/storybook/issues/11376#issuecomment-757417250

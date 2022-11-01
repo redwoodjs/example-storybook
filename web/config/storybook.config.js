99a61d5307f273c9cc3ce864bec4ac9fa9c3fafd
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2022-11-01 18:11:29 -0400

    chore(@storybook/addon-coverage): configure
    https://github.com/yannbf/storybook-coverage-recipes/tree/main/angular#setting-up-coverage

--- web/config/storybook.config.js
+++ web/config/storybook.config.js
@@ -12,4 +12,26 @@ module.exports = {
       rwjsPaths.web.src
     )}/stories/Introduction.stories.mdx`,
   ],
+  webpackFinal: async (config) => {
+    const rules = config.module?.rules || []
+    rules.push({
+      test: /\.(js|ts)$/,
+      loader: 'istanbul-instrumenter-loader',
+      options: { esModules: true },
+      enforce: 'post',
+      include: require('path').join(__dirname, '..', 'src'),
+      exclude: [
+        /\.(e2e|spec|stories)\.ts$/,
+        /node_modules/,
+        /polyfills\.ts/,
+        /preview\.js/,
+        /(ngfactory|ngstyle)\.js/,
+      ],
+    })
+
+    config.module = config.module || {}
+    config.module.rules = rules
+
+    return config
+  },
 }

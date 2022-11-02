705081133ad61171c7886aa2c40f88dd7c802a85
Peter Colapietro <petercolapietro+github@gmail.com>
2022-11-01 20:28:20 -0400

    feat(storybook): add `@storybook/addon-coverage` (#109)
    * feat(storybook): add `@storybook/addon-coverage`
    
    https://github.com/storybookjs/test-runner#using-storybookaddon-coverage
    https://github.com/yannbf/storybook-coverage-recipes
    
    * feat(@storybook/addon-coverage): `--coverage`
    
    see: https://github.com/storybookjs/test-runner#2---run-tests-with---coverage-flag
    
    * chore: bring webpack merge code in
    
    see: https://github.com/redwoodjs/redwood/pull/5957/files
    
    * build(deps): add `istanbul-instrumenter-loader`
    
    * chore(@storybook/addon-coverage): configure
    
    https://github.com/yannbf/storybook-coverage-recipes/tree/main/angular#setting-up-coverage
    
    * build(github): store coverage report

--- web/config/storybook.config.js
+++ web/config/storybook.config.js
@@ -6,10 +6,32 @@ module.exports = {
   features: {
     interactionsDebugger: true,
   },
-  addons: ['@storybook/addon-interactions'],
+  addons: ['@storybook/addon-interactions', '@storybook/addon-coverage'],
   stories: [
     `${importStatementPath(
       rwjsPaths.web.src
     )}/stories/Introduction.stories.mdx`,
   ],
+  webpackFinal: async (config) => {
+    const rules = config.module?.rules || []
+    rules.push({
+      test: /\.(js|ts)$/,
+      loader: 'istanbul-instrumenter-loader',
+      options: { esModules: true },
+      enforce: 'post',
+      include: require('path').join(__dirname, '..', 'src'),
+      exclude: [
+        /\.(e2e|spec|stories)\.ts$/,
+        /node_modules/,
+        /polyfills\.ts/,
+        /preview\.js/,
+        /(ngfactory|ngstyle)\.js/,
+      ],
+    })
+
+    config.module = config.module || {}
+    config.module.rules = rules
+
+    return config
+  },
 }

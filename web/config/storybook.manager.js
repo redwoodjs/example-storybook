155728a987a93332a82711d1dfce6fe7a0dad026
Peter Colapietro <petercolapietro+github@gmail.com>
2022-11-06 12:17:22 -0500

    chore(storybook): minor tweaks (#193)
    prepare for MDX2

--- web/config/storybook.manager.js
+++ web/config/storybook.manager.js
@@ -1,6 +1,10 @@
 import { addons } from '@storybook/addons'
+
 import RedwoodTheme from './redwood.storybook.theme'
 
+const theme = RedwoodTheme
 addons.setConfig({
-  theme: RedwoodTheme,
+  theme,
 })
+
+export { theme }

2ec7a21daa468436fc22d73989add3b508569e92
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2023-03-19 14:51:04 -0400

    chore(storybook): wire up mocks in production storybook builds
    see: https://github.com/redwoodjs/redwood/issues/5534\#issuecomment-1474103316

--- web/config/storybook.config.js
+++ web/config/storybook.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack')
+
 const { importStatementPath, getPaths } = require('@redwoodjs/internal')
 
 const rwjsPaths = getPaths()
@@ -12,7 +14,7 @@ module.exports = {
       rwjsPaths.web.src
     )}/stories/Introduction.stories.mdx`,
   ],
-  webpackFinal: async (config) => {
+  webpackFinal: async (config, { configType }) => {
     const rules = config.module?.rules || []
     rules.push({
       test: /\.(js|ts)$/,
@@ -31,6 +33,19 @@ module.exports = {
 
     config.module = config.module || {}
     config.module.rules = rules
+
+    // see: https://github.com/redwoodjs/redwood/issues/5534#issuecomment-1474103316
+    configType === 'PRODUCTION' &&
+      config.plugins.push(
+        new webpack.ProvidePlugin({
+          mockGraphQLQuery: ['@redwoodjs/testing/web', 'mockGraphQLQuery'],
+          mockGraphQLMutation: [
+            '@redwoodjs/testing/web',
+            'mockGraphQLMutation',
+          ],
+          mockCurrentUser: ['@redwoodjs/testing/web', 'mockCurrentUser'],
+        })
+      )
 
     return config
   },
